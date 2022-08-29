e39a71117e031155892ad99a497e71b7612eaad7
Mariana Machohan <debrisofme9@gmail.com>
2022-08-29 09:10:40 +0200

    1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@
 import './App.css';
 import Homework1 from './Homework1'
 
+const Homework1Data = {
+	p1 : 88,
+}
+
 function App(props, state) {
   return (
 		<>
-			<Homework1 />
+			<Homework1 data={Homework1Data}/>
 		
 		</>
   );
