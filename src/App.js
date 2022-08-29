573284e47e39da8da870c78153a84d6d7b648633
Mariana Machohan <debrisofme9@gmail.com>
2022-08-29 08:37:54 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import Homevork1 from './Homework1'
+import Homework1 from './Homework1'
 
 function App(props, state) {
   return (
 		<>
-			<Homevork1 />
+			<Homework1 />
 		
 		</>
   );
